8fd0d570276dd3c1dc67d50b699577640917d51b
noname1015 <noname_1015@hotmail.com>
2023-02-10 11:41:16 +0800

    Add files via upload
    First version commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace CalculateMathExpr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtExpr = new System.Windows.Forms.RichTextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtExpr
+            // 
+            this.txtExpr.Location = new System.Drawing.Point(12, 35);
+            this.txtExpr.Name = "txtExpr";
+            this.txtExpr.Size = new System.Drawing.Size(362, 98);
+            this.txtExpr.TabIndex = 1;
+            this.txtExpr.Text = "";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Enabled = false;
+            this.txtResult.Location = new System.Drawing.Point(12, 211);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(362, 20);
+            this.txtResult.TabIndex = 2;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(12, 139);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(362, 48);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Math Expression";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 243);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtExpr);
+            this.Name = "Form1";
+            this.Text = "Math Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtExpr;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace CalculateMathExpr
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            int error;
+            string result = CalculateMathExpr(txtExpr.Text, out error).ToString();
+
+            if (error >= 0) txtResult.Text = result;
+
+        }
+
+        public double CalculateMathExpr(string expr, out int errCode)
+        {
+            errCode = 0;
+
+            Stack<double> stkNumbers = new Stack<double>(); // Only store numbers in stack
+            List<string> liststrPrioritizedBlock = PrioritizeMathBlock(expr);
+           
+            double number;
+
+            //Final calculation by using prioritized list
+            foreach (string block in liststrPrioritizedBlock)
+            {
+                if (double.TryParse(block, out number))
+                {
+                    stkNumbers.Push(number);
+                }
+                else
+                {
+                    double num1 = stkNumbers.Pop();
+                    double num2 = stkNumbers.Pop();
+
+                    switch (block)
+                    {
+                        case "+":
+                            stkNumbers.Push(num2 + num1);
+                            break;
+                        case "-":
+                            stkNumbers.Push(num2 - num1);
+                            break;
+                        case "*":
+                            stkNumbers.Push(num2 * num1);
+                            break;
+                        case "/":
+                            stkNumbers.Push(num2 / num1);
+                            break;
+                    }
+                }
+            }
+            if (liststrPrioritizedBlock.Count == 0)
+            {
+                errCode = -1;
+                return -1;
+            }
+
+            return stkNumbers.Pop();
+        }
+
+        /// <summary>
+        /// Prioritize all numbers and operator in sequence first before final calculation with priority
+        /// </summary>
+        /// <param name="expr">Math expression</param>
+        /// <returns></returns>
+        private List<string> PrioritizeMathBlock(string expr)
+        {
+            List<string> liststrPrioBlock = new List<string>(); //Require store list without fifo concept
+            Stack<string> stkOperator = new Stack<string>(); //use to store temp operator for analysis purpose
+            Regex validateNumberRegex = new Regex(@"-?\d+(?:\.\d+)?|\+|\-|\*|\/|[(]|[)]");
+            //Regex validateOpr = new Regex(@"\+|\-|\*|\/");
+
+            string[] blocks = expr.Split(' ');
+            double tryDoub;
+            foreach (string block in blocks)
+            {
+                //Exception scenario
+                if (!validateNumberRegex.IsMatch(block))
+                {
+                    txtResult.Text = "Wrong math expression";
+                    return new List<string>();
+                }
+
+
+                if (double.TryParse(block, out tryDoub))
+                {
+                    liststrPrioBlock.Add(block);
+                }
+                else if (block == "(")
+                {
+                    stkOperator.Push(block);
+                }
+                else if (block == ")")
+                {
+                    while (stkOperator.Peek() != "(")
+                    {
+                        liststrPrioBlock.Add(stkOperator.Pop());
+                    }
+                    stkOperator.Pop();
+                }
+                else
+                {
+                    //Once operator +-*/ found, loop until all temp operator finish prioritize
+                    while (stkOperator.Count > 0 && GetPriority(block) <= GetPriority(stkOperator.Peek()))
+                    {
+                        //add into final prioritized list if previous operator is analyzed (Non-bracket)
+                        liststrPrioBlock.Add(stkOperator.Pop());
+                    }
+                    stkOperator.Push(block);
+                }
+            }
+
+            while (stkOperator.Count > 0)
+            {
+                liststrPrioBlock.Add(stkOperator.Pop());
+            }
+
+            return liststrPrioBlock;
+        }
+
+        /// <summary>
+        /// Provide priority for operator to reorganize sequence
+        /// </summary>
+        /// <param name="op"></param>
+        /// <returns></returns>
+        private int GetPriority(string op)
+        {
+            switch (op)
+            {
+                case "+":
+                case "-":
+                    return 1;
+                case "*":
+                case "/":
+                    return 2;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
